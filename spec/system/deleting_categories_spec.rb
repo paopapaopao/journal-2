de1d2a2dd076eeb72533d101f43b07a222cd61df
paopapaopao <paolo.casugay@gmail.com>
2021-08-04 22:07:56 +0800

    Refactor: delete category system test

--- spec/system/deleting_categories_spec.rb
+++ spec/system/deleting_categories_spec.rb
@@ -9,21 +9,33 @@ RSpec.describe "DeletingCategories", type: :system do
   let(:category_count) { Category.count }
   let(:category) { Category.find_by(title: 'Category Title') }
 
-  it 'deletes a category' do
+  it "redirects to the 'new' page" do
     visit root_path
     click_on 'New Category'
-
     expect(page).to have_current_path(new_category_path)
+  end
+
+  it 'redirects to the created category' do
     fill_in 'Title', with: 'Category Title'
     fill_in 'Description', with: 'Category Description'
     click_on 'Create Category'
-
     expect(page).to have_current_path(category_path(id))
-    expect(page).to have_content('Category Title')
-    expect(page).to have_content('Category Description')
+  end
 
+  it 'page shows category title' do
+    expect(page).to have_content('Category Title')
+  end
+
+  it 'page shows category description' do
+    expect(page).to have_content('Category Description')
+  end
+
+  it 'decreases category count by 1' do
     click_link 'Delete'
     expect(category_count).to eq 0
+  end
+
+  it 'category is deleted' do
     expect(category).to eq nil
   end
 
