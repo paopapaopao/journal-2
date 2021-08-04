2796453c69880b69a272672ec09e1f9446b9454c
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-04 10:14:43 +0800

    Update test for creating categories

--- spec/system/creating_categories_spec.rb
+++ spec/system/creating_categories_spec.rb
@@ -5,5 +5,27 @@ RSpec.describe "CreatingCategories", type: :system do
     driven_by(:rack_test)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  let(:id) { Category.find_by(title: 'Category Title').id }
+  let(:category_count) { Category.count }
+  let(:category) { Category.find_by(title: 'Category Title') }
+
+  it 'creates a category' do
+    visit root_path
+    click_on 'New Category'
+
+    expect(page).to have_current_path(new_category_path)
+    fill_in 'Title', with: 'Category Title'
+    fill_in 'Description', with: 'Category Description'
+    click_on 'Create Category'
+
+    expect(page).to have_current_path(category_path(id))
+    expect(page).to have_content('Category Title')
+    expect(page).to have_content('Category Description')
+
+    expect(category_count).to eq 1
+    expect(category.title).to eq('Category Title')
+    expect(category.description).to eq('Category Description')
+  end
+
+  # pending "add some scenarios (or delete) #{__FILE__}"
 end
