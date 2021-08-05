3eed0750635564206abcfc5ec531c136d68db57d
paopapaopao <paolo.casugay@gmail.com>
2021-08-05 22:13:28 +0800

    Add: has many tasks association for category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true,
                           length: { minimum: 10, maximum: 100 }
+
+  has_many :tasks
 end
