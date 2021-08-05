013f6dd05087c2e21530da98da444597ffb21cd0
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-05 21:37:34 +0800

    Add maximum validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
   validates :description, presence: true,
-                          length: { minimum: 10 }
+                          length: { minimum: 10, maximum: 100 }
 end
