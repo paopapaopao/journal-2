6c87b07e976d8a83c9dc882cc767376fdb68ad02
paopapaopao <paolo.casugay@gmail.com>
2021-08-06 00:10:03 +0800

    Update: destroy method for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -32,6 +32,10 @@ class CategoriesController < ApplicationController
   end
 
   def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+
+    redirect_to categories_path
   end
 
   private
