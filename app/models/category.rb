3e7592361ef839af06db92fee9cdee66fd6667d6
paopapaopao <paolo.casugay@gmail.com>
2021-08-05 21:10:31 +0800

    Add: description attribute for category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   validates :title, presence: true, uniqueness: true
+  validates :description, presence: true,
+                          length: { minimum: 10, maximum: 100 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 2021_08_05_123826) do
 
   create_table "categories", force: :cascade do |t|
     t.string 'title'
+    t.string 'description'
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
