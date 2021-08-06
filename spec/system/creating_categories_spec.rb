d083592bba9c3ff9ab9f14362c2b86ed5d4f66b8
paopapaopao <paolo.casugay@gmail.com>
2021-08-06 10:04:16 +0800

    Update: create categories system test

--- spec/system/creating_categories_spec.rb
+++ spec/system/creating_categories_spec.rb
@@ -60,6 +60,13 @@ RSpec.describe "CreatingCategories", type: :system do
   end
 
   it "category.title == 'Category Title'" do
+    visit root_path
+    click_on 'New Category'
+
+    fill_in 'Title', with: 'Category Title'
+    fill_in 'Description', with: 'Category Description'
+    click_on 'Create Category'
+
     expect(category.title).to eq('Category Title')
   end
 
