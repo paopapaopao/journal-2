7562148bf7089b2d24df507efb449ba1dbf70512
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-06 19:23:09 +0800

    Update test for viewing tasks

--- spec/system/viewing_tasks_spec.rb
+++ spec/system/viewing_tasks_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe 'ViewingTasks', type: :system do
     driven_by(:rack_test)
   end
 
+  let(:date_today) { Date.today }
   let(:category_id) { Category.find_by(title: 'Category Title').id }
-  let(:click_show_category) { find("a[href='/categories/#{category_id}']").click }
 
-  it 'views a task' do
+  before :each do
     visit root_path
     click_on 'New Category'
 
@@ -21,9 +21,15 @@ RSpec.describe 'ViewingTasks', type: :system do
 
     visit root_path
     click_link 'Category Title'
+  end
 
+  it 'redirects to its category' do
     expect(page).to have_current_path(category_path(category_id))
+  end
+
+  it 'renders page with its contents' do
     expect(page).to have_content('Task Description')
+    expect(page).to have_content(date_today)
   end
 
   # pending "add some scenarios (or delete) #{__FILE__}"
