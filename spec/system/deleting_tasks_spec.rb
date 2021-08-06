201204a9903fc6f0b7536c0b8e261bcc21ebd589
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-06 19:22:41 +0800

    Update test for deleting tasks

--- spec/system/deleting_tasks_spec.rb
+++ spec/system/deleting_tasks_spec.rb
@@ -5,10 +5,13 @@ RSpec.describe 'DeletingTasks', type: :system do
     driven_by(:rack_test)
   end
 
-  let(:category_id) { Category.find_by(title: 'Category Title').id }
-  let(:task_id) { Task.find_by(details: 'Task Details').id }
   let(:click_destroy_task) { find("a[href='/categories/#{category_id}/tasks/#{task_id}']").click }
+
+  let(:category_id) { Category.find_by(title: 'Category Title').id }
+
+  let(:task_id) { Task.find_by(details: 'Task Details').id }
   let(:task_count) { Task.count }
+
   let(:task) { Task.find_by(details: 'Task Details') }
 
   it 'deletes a task' do
