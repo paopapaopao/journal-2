e0fdba515dadf0b175a07157183a45a271280f6f
paopapaopao <paolo.casugay@gmail.com>
2021-08-07 12:30:14 +0800

    Update: migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_06_124502) do
+ActiveRecord::Schema.define(version: 2021_08_07_042228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2021_08_06_124502) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
@@ -27,7 +29,9 @@ ActiveRecord::Schema.define(version: 2021_08_06_124502) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "description"
     t.bigint "category_id", null: false
+    t.bigint "user_id", null: false
     t.index ["category_id"], name: "index_tasks_on_category_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,5 +46,7 @@ ActiveRecord::Schema.define(version: 2021_08_06_124502) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "users"
   add_foreign_key "tasks", "categories"
+  add_foreign_key "tasks", "users"
 end
