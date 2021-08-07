ae5371841c485db1d0c924d8fa23dfdce178fc59
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-08 03:05:56 +0800

    Remove turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
@@ -12,5 +12,5 @@ import "bootstrap"
 import "jquery-ui"
 
 Rails.start()
-Turbolinks.start()
+// Turbolinks.start()
 ActiveStorage.start()
