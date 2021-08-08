3ab5cf9fa47bfb9c1c42ed637a3e46f9d1bdd741
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-08 17:15:57 +0800

    Update test for deleting tasks

--- spec/system/deleting_tasks_spec.rb
+++ spec/system/deleting_tasks_spec.rb
@@ -3,41 +3,43 @@ require 'rails_helper'
 RSpec.describe 'DeletingTasks', type: :system do
   before do
     driven_by(:rack_test)
+    category
   end
 
-  let(:date_today) { Date.today }
+  let(:category) { Category.create(title: 'Category Title', description: 'Category Description') }
 
-  let(:category_create) { Category.create(title: 'Category Title', description: 'Category Description') }
-  let(:category_id) { Category.find_by(title: 'Category Title').id }
-
-  let(:task_create) { Task.create(description: 'Task Description', priority: date_today, category_id: category_id) }
-  let(:task_id) { Task.find_by(description: 'Task Description').id }
-  let(:task_count) { Task.count }
-  let(:task_deleted) { Task.find_by(description: 'Task Description') }
-
-  let(:click_destroy_task) { find("a[href='/categories/#{category_id}/tasks/#{task_id}']").click }
-
-  before :each do
-    category_create
-    task_create
-
-    visit category_path(category_id)
-  end
-
-  context 'when task was created within its category' do
-    it 'redirects to same page' do
-      expect(page).to have_current_path(category_path(category_id))
+  describe Task do
+    let(:attributes) do
+      {
+        description: 'Task Description',
+        priority: Date.today,
+        category_id: category.id
+      }
     end
-  end
 
-  context 'with that task can be deleted' do
+    subject { described_class.create(attributes) }
+    let(:click_destroy) { find("a[href='/categories/#{category.id}/tasks/#{subject.id}']").click }
+
     before do
-      click_destroy_task
+      subject
+      visit category_path(category)
     end
 
-    it 'deletes the task' do
-      expect(task_deleted).to eq nil
-      expect(task_count).to eq 0
+    context 'when task was created within its category' do
+      it 'redirects to same page' do
+        expect(page).to have_current_path(category_path(category))
+      end
+    end
+
+    context 'with that task can be deleted' do
+      before do
+        click_destroy
+      end
+
+      it 'deletes the task' do
+        expect(Task.find_by(attributes)).to eq nil
+        expect(Task.count).to eq 0
+      end
     end
   end
 
