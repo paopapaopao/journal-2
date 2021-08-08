f5498922aed9ba526ae3e4f0535259490e8150e3
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-08 17:16:27 +0800

    Update test for viewing tasks

--- spec/system/viewing_tasks_spec.rb
+++ spec/system/viewing_tasks_spec.rb
@@ -5,15 +5,21 @@ RSpec.describe 'ViewingTasks', type: :system do
     driven_by(:rack_test)
   end
 
-  let(:date_today) { Date.today }
+  let(:category) { Category.create(title: 'Category Title', description: 'Category Description') }
 
-  let(:category_create) { Category.create(title: 'Category Title', description: 'Category Description') }
-  let(:category_id) { Category.find_by(title: 'Category Title').id }
+  let(:task_yesterday) do
+    Task.new(description: 'Task Description', priority: Date.yesterday,
+             category_id: category.id).save(validate: false)
+  end
 
-  let(:task_create) { Task.create(description: 'Task Description', priority: date_today, category_id: category_id) }
+  let(:task_today) { Task.create(description: 'Task Description', priority: Date.today, category_id: category.id) }
+
+  let(:task_tomorrow) do
+    Task.create(description: 'Task Description', priority: Date.tomorrow, category_id: category.id)
+  end
 
   context 'when navigating in page of all categories' do
-    before :each do
+    before do
       visit categories_path
     end
 
@@ -28,9 +34,9 @@ RSpec.describe 'ViewingTasks', type: :system do
     end
 
     context 'when a task for today was created' do
-      before :each do
-        category_create
-        task_create
+      before do
+        category
+        task_today
       end
 
       it 'shows heading' do
@@ -44,14 +50,14 @@ RSpec.describe 'ViewingTasks', type: :system do
   end
 
   context 'when navigating through tasks inside a category' do
-    before :each do
-      category_create
+    before do
+      category
       visit categories_path
     end
 
     context 'when there are no tasks yet' do
-      before :each do
-        click_link 'Category Title'
+      before do
+        click_on 'Category Title'
       end
 
       context 'when navigating under "Overdue Tasks"' do
@@ -85,19 +91,53 @@ RSpec.describe 'ViewingTasks', type: :system do
       end
     end
 
-    context 'when a task was created' do
-      before :each do
-        task_create
-        click_link 'Category Title'
+    context 'when a task for today was created' do
+      before do
+        task_today
+        click_on 'Category Title'
       end
 
       it 'redirects to its category' do
-        expect(page).to have_current_path(category_path(category_id))
+        expect(page).to have_current_path(category_path(category))
       end
 
       context 'when navigating under "Tasks for Today"' do
         it 'shows created task' do
           within('#today-wrap') { expect(page).to have_content('Task Description') }
+        end
+      end
+    end
+
+    context 'when a future task was created' do
+      before do
+        task_tomorrow
+        click_on 'Category Title'
+      end
+
+      it 'redirects to its category' do
+        expect(page).to have_current_path(category_path(category))
+      end
+
+      context 'when navigating under "Future Tasks"' do
+        it 'shows created task' do
+          within('#future-wrap') { expect(page).to have_content('Task Description') }
+        end
+      end
+    end
+
+    context 'when a task was overdue' do
+      before do
+        task_yesterday
+        click_on 'Category Title'
+      end
+
+      it 'redirects to its category' do
+        expect(page).to have_current_path(category_path(category))
+      end
+
+      context 'when navigating under "Overdue Tasks"' do
+        it 'shows created task' do
+          within('#overdue-wrap') { expect(page).to have_content('Task Description') }
         end
       end
     end
