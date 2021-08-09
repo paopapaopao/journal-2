ff279ff6e9fe2baf45b86d81e080b57abfbfacb3
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-09 18:29:19 +0800

    Update test for editing tasks

--- spec/system/editing_tasks_spec.rb
+++ spec/system/editing_tasks_spec.rb
@@ -3,92 +3,111 @@ require 'rails_helper'
 RSpec.describe 'EditingTasks', type: :system do
   before do
     driven_by(:rack_test)
-    category
   end
 
-  let(:category) { Category.create(title: 'Category Title', description: 'Category Description') }
+  let(:user) do
+    User.create(email: 'example@mail.com',
+                password: 'password')
+  end
+
+  let(:category) do
+    Category.create(title: 'Category Title',
+                    description: 'Category Description',
+                    user_id: user.id)
+  end
 
   describe Task do
     let(:attributes) do
       {
         description: 'Task Description',
         priority: Date.today,
+        user_id: user.id,
         category_id: category.id
       }
     end
 
     subject { described_class.create(attributes) }
-    let(:subject_find) { Task.find_by(attributes) }
-    let(:subject_find_edited) { Task.find_by(description: 'Task Description Edited') }
 
     let(:click_edit) { find("a[href='/categories/#{category.id}/tasks/#{subject.id}/edit']").click }
+    let(:click_update) { find('input[type="submit"]').click }
 
-    before do
-      subject
-      visit category_path(category)
-      click_edit
-    end
-
-    context 'when task was created within its category, with priority date was date today then it can be edited' do
-      it 'goes to form' do
-        expect(page).to have_current_path(edit_category_task_path(category, subject))
-      end
-    end
-
-    context 'when all form fields were filled up with new inputs and submitted with priority date was date tomorrow' do
-      let(:date_tomorrow) { Date.tomorrow }
-
+    context 'when user logged in' do
       before do
-        fill_in 'Description', with: 'Task Description Edited'
-        fill_in 'task[priority]', with: date_tomorrow
-        click_on 'Update Task'
+        sign_in user
+        category
+        subject
+        visit category_path(category)
+        click_edit
       end
 
-      it 'updates task' do
-        expect(subject_find_edited.details).to eq('Task Description Edited')
-        expect(subject_find_edited.priority).to eq(date_tomorrow)
+      context 'when task was created within its category, with priority date was date today then it can be edited' do
+        it 'goes to form' do
+          expect(page).to have_current_path(edit_category_task_path(category, subject))
+        end
       end
 
-      it 'redirects to its category' do
-        expect(page).to have_current_path(category_path(category))
+      context 'when all form fields were filled up with new inputs and submitted' do
+        let(:date_tomorrow) { Date.tomorrow }
+
+        let(:subject_find_edited) do
+          Task.find_by(description: 'Task Description Edited',
+                       priority: date_tomorrow)
+        end
+
+        before do
+          fill_in 'Description', with: 'Task Description Edited'
+          fill_in 'task[priority]', with: date_tomorrow
+          click_update
+        end
+
+        it 'updates task' do
+          expect(subject_find_edited).to_not eq nil
+        end
+
+        it 'redirects to its category' do
+          expect(page).to have_current_path(category_path(category))
+        end
+
+        context 'when navigating under "Future Tasks"' do
+          it 'shows updated task' do
+            within('#future-wrap') { expect(page).to have_content(subject_find_edited.description) }
+          end
+        end
       end
 
-      context 'when navigating under "Future Tasks"' do
-        it 'shows updated task' do
-          within('#future-wrap') { expect(page).to have_content('Task Description Edited') }
+      context 'when all form fields were filled up with blank inputs and submitted' do
+        let(:subject_find) { Task.find_by(attributes) }
+
+        before do
+          fill_in 'Description', with: ''
+          fill_in 'task[priority]', with: ''
+          click_update
+        end
+
+        it 'does not update task' do
+          expect(subject_find).to_not eq nil
+        end
+
+        it 'redirects back to form' do
+          expect(page).to have_current_path(category_task_path(category, subject))
+        end
+
+        it 'raises error' do
+          expect(page).to have_css('#form-error-wrap')
         end
       end
     end
 
-    context 'when all form fields were filled up with blank inputs and submitted' do
+    context 'when user not logged in' do
       before do
-        fill_in 'Description', with: ''
-        fill_in 'task[priority]', with: ''
-        click_on 'Update Task'
+        category
+        subject
+        visit edit_category_task_path(category, subject)
       end
 
-      it 'does not update task' do
-        expect(subject_find.details).to eq('Task Description')
-        expect(subject_find.priority).to eq(Date.today)
-      end
-
-      it 'redirects back to form' do
-        expect(page).to have_current_path(category_task_path(category, subject))
-      end
-
-      context 'with description blank' do
-        it 'raises an error' do
-          expect(page).to have_content('blank')
-        end
-      end
-
-      context 'with priority date blank and considered in the past' do
-        it 'raises an error' do
-          expect(page).to have_content('minimum')
-        end
+      it 'shows "not allowed" message' do
+        expect(page).to have_content('not allowed')
       end
     end
   end
-
-  # pending "add some scenarios (or delete) #{__FILE__}"
 end
